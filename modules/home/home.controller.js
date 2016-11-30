8421d1905ccd991b9416abb6eff9615af0801849
Ashish_Dwivedi04 <Ashish_Dwivedi04@HYDPCM266247D.ad.infosys.com>
2016-11-30 10:20:28 +0530

    New fields added

--- modules/home/home.controller.js
+++ modules/home/home.controller.js
@@ -27,10 +27,18 @@ Author: Ashish Dwivedi
 
 		function switchLayout() {
 			_this.layoutHorizontal = !_this.layoutHorizontal;
+			$('.home.name-header').css({'line-height' : '100px', 'transition' : 'all ease-in 0.3s'});
 		}
 
 		function switchColor() {
 			_this.pickColor = !_this.pickColor;
+			if($(".social-connect").hasClass('animation')) {
+				$('.social-connect').css({'left' : '50vw', 'animation' : ''});
+				$(".social-connect").removeClass("animation");
+			} else {
+				$('.social-connect').css({'left' : '105vw', 'animation' : 'rotatecolor',  'transition' : 'all ease-in 0.3s'});
+				$(".social-connect").addClass("animation");
+			}
 		}
 	}
 })();
