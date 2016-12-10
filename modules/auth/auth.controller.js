a0def11094c1742d6900d66465e66d71b87b7629
Ashish_Dwivedi04 <Ashish_Dwivedi04@HYDPCM266247D.ad.infosys.com>
2016-12-10 13:29:14 +0530

    Added works module

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -3,9 +3,9 @@
 	angular.module('My.Auth')
 	.controller('AuthController', AuthController);
 
-	AuthController.$inject = ['$location'];
+	AuthController.$inject = ['$location', '$http', '$timeout'];
 
-	function AuthController($location) {
+	function AuthController($location, $http, $timeout) {
 		var _this = this;
 		_this.creds = {
 			username: '',
@@ -19,15 +19,29 @@
 		$('#authModal').modal({backdrop: 'static', keyboard: false, show: true});
 
 		function login() {
-			if(_this.creds) {
-				if(_this.creds.username === 'ashish' && _this.creds.password === 'ashish') {
-					$('#authModal').modal('hide');
-					sessionStorage.setItem('username', _this.creds.username);
-					$location.path('/');
-				} else {
-					_this.errorText = 'Please provide valid credentials';
-				}
+			if(_this.creds.username && _this.creds.password) {
+				$http.get('../../scripts/auth.data.json').then(function(result) {
+					if(result.data.authData.username.indexOf(_this.creds.username)!==-1) {
+						if(result.data.authData.password[result.data.authData.username.indexOf(_this.creds.username)] ===_this.creds.password) {
+							$('#authModal').modal('hide');
+							sessionStorage.setItem('username', _this.creds.username);
+							$location.path('/');
+						}
+						else {
+						_this.errorText = 'Please provide valid credentials';
+						}
+					} else {
+						_this.errorText = 'Username does not exist in the database';
+					}
+				}, function(error) {
+					_this.errorText = 'Could not authenticate, please try again after reloading the page';
+				})
+			} else {
+				_this.errorText = 'Credentials are manatory fields. Please input values!';
 			}
+			$timeout(function() {
+				_this.errorText = '';
+			}, 3000);
 		}
 	}
 })();
--- modules/works/works.controller.js
+++ modules/works/works.controller.js
@@ -3,9 +3,24 @@
 	angular.module('My.Works')
 	.controller('WorksController', WorksController);
 
-	WorksController.$inject = [];
+	WorksController.$inject = ['$http'];
 
-	function WorksController() {
+	function WorksController($http) {
 		var _this = this;
+		//Variables
+		_this.myProjects = [];
+
+		//Functions
+
+		function initialize() {
+			//Get my works data from the JSON
+			$http.get('../../scripts/my.works.json').then(function(result) {
+				_this.myProjects = result.data.works;
+			}, function(error) {
+				console.log('Something went wrong, please try again');
+			});
+		}
+
+		initialize();
 	}
 })();
