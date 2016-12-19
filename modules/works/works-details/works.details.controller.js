5287418f3789c48006fcfcd00fb2fbe601321c19
ashish_dwivedi04 <ashish_dwivedi04@HYDPCM266247D.ad.infosys.com>
2016-12-19 19:54:28 +0530

    Works details page implemented.

--- modules/works/works-details/works.details.controller.js
+++ modules/works/works-details/works.details.controller.js
@@ -14,6 +14,7 @@
 		_this.project = {};
 
 		//Functions
+		_this.goBack = goBack;
 
 		function initialize() {
 			$http.get('scripts/my.works.json').then(function (result) {
@@ -23,10 +24,24 @@
 					if(project.id === currentProjectId) {
 						_this.project = project;
 					}
-				})
+				});
+				$http.get('scripts/my.works.desc.json').then( function (result) {
+					_this.project['details'] = result.data.works.filter( function (item) {
+						return item.id === _this.project.id;
+					}).map( function (item) {
+						return item.description;
+					});
+					_this.project['details'] = _this.project['details'][0];
+				}, function (error) {
+					console.log('Something went wrong, please try again');
+				});
 			}, function (error) {
 				console.log('Something went wrong while fetching records. Please try later!');
 			});
+		}
+
+		function goBack() {
+			history.go(-1);
 		}
 
 		initialize();
